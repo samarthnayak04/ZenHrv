2a7c158cd1116f3424fdf164876306c73978ce14
samarthnayak04 <samarthnayak04@gmail.com>
2025-07-27 19:04:05 +0530

    update config

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "../styles/Login.css";
+import "../styles/login.css";
 
 function Login() {
   const navigate = useNavigate();
